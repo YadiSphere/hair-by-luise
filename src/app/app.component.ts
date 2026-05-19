d541476828f45ee3ca5c6c7f740d241261516d42
Yadigar <YadiSphere@github.com>
2026-05-19 20:31:45 +0200

    feature: Add hero with titel text, images and side text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component'; 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['../styles.css']
 })
 export class AppComponent {
-  title = 'first';
+  title = 'frontend';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+
 
 export const routes: Routes = [];
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./../styles.css',]
+})
+export class AppComponent {
+  title = 'frontend';
+}
